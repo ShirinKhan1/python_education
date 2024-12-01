fdcf48ac5ec7fba0f1e3fbae744ebb90aadfdbee
Ramis <102373473+ShirinKhan1@users.noreply.github.com>
2024-12-01 16:22:40 +0300

    сделал 3 задание в 5 ДЗ

--- homework_5/task_1_2_3.py
+++ homework_5/task_1_2_3.py
@@ -20,6 +20,13 @@ def fetch_products_in_price_range():
     FROM products
     WHERE category_id = 1;
     """
+    query3 = """
+    SELECT supplier_id, MAX(unit_price) AS max_price
+    FROM products
+    WHERE supplier_id IN (1, 3, 5)
+    GROUP BY supplier_id
+    ORDER BY supplier_id;
+    """
 
     try:
         # Подключение к базе данных
@@ -29,6 +36,8 @@ def fetch_products_in_price_range():
                 results1 = cursor.fetchall()
                 cursor.execute(query2)
                 results2 = cursor.fetchall()
+                cursor.execute(query3)
+                results3 = cursor.fetchall()
 
         # Вывод результатов
         print("Продукты с ценой от 3 до 7:")
@@ -38,6 +47,12 @@ def fetch_products_in_price_range():
             print(f"Цена самого дешевого товара в категории 1: {results2[0]}")
         else:
             print("Нет товаров в категории 1.")
+        if results3:
+            print("Максимальная цена по поставщикам:")
+            for row in results3:
+                print(f"Supplier ID: {row[0]}, Max Price: {row[1]}")
+        else:
+            print("Нет данных для поставщиков с id 1, 3, 5.")
 
     except psycopg2.Error as e:
         print(f"Ошибка подключения или выполнения запроса: {e}")
